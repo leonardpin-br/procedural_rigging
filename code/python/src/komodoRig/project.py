1e588a712b326d524b54b0f69e1c80f436e5845d
Leonardo Pinheiro <60147713+leonardpin-br@users.noreply.github.com>
2023-01-19 11:24:22 -0300

    Improved path

--- code/python/src/komodoRig/project.py
+++ code/python/src/komodoRig/project.py
@@ -3,5 +3,19 @@
 u"""main project file with central variables
 """
 
+import os
+
+cwd = os.getcwd()
+cwd = cwd.replace("\\", "/")
+
+# Dealing with Sphinx limitation:
+if "sphinx" in cwd:
+    pathList = cwd.split("/")
+
+    # Removing "docs" and "sphinx" form path:
+    pathList = pathList[:-2]
+
+    cwd = "/".join(pathList)
+
 sceneScale = 1.0
-mainProjectPath = "E:/cloud/Videoaulas/Digital Tutors - Procedural Rigging with Python in Maya/procedural_rigging/assets"
+mainProjectPath = os.path.join(cwd, "assets").replace("\\", "/")
