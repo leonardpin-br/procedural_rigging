42d426b7adc39e5acf1bb0fc75d39592c1bdfa83
Leonardo Pinheiro <60147713+leonardpin-br@users.noreply.github.com>
2023-01-20 22:34:33 -0300

    Updated doc in main file

--- code/python/src/procedural_rigging.py
+++ code/python/src/procedural_rigging.py
@@ -5,8 +5,12 @@ u"""Main file used to run the application.
 References:
     `3.7 Shebang Line`_
 
+    `unable to run 'aws' from cygwin`_
+
 .. _3.7 Shebang Line:
    https://google.github.io/styleguide/pyguide.html#37-shebang-line
+.. _unable to run \'aws\' from cygwin:
+   https://stackoverflow.com/a/62093489/3768670
 """
 
 import sys
