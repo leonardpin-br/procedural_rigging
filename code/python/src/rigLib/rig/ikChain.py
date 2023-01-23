62c3f1230bc73641ce2c6f5467320b0c500090dc
Leonardo Pinheiro <60147713+leonardpin-br@users.noreply.github.com>
2023-01-23 19:58:45 -0300

    After 39

--- code/python/src/rigLib/rig/ikChain.py
+++ code/python/src/rigLib/rig/ikChain.py
@@ -16,7 +16,7 @@ def build(chainJoints,
           fkParenting=True,
           baseRig=None
           ):
-    """_summary_
+    u"""_summary_
 
     Args:
         chainJoints (list[str]): List of chain joints.
@@ -30,3 +30,38 @@ def build(chainJoints,
     Returns:
         Dictionary with rig module objects.
     """
+
+    # make rig module
+    rigmodule = module.Module(prefix=prefix, baseObj=baseRig)
+
+    # make chain curve clusters
+    chainCurveCVs = mc.ls("{}.cv[*]".format(chainCurve), fl=1)
+    numChainCVs = len(chainCurveCVs)
+    chainCurveClusters = []
+
+    for i in range(numChainCVs):
+        clusterWithNumber = "Cluster%d" % (i + 1)
+        cls = mc.cluster(chainCurveCVs[i], n="{}{}".format(prefix, clusterWithNumber))[1]
+        chainCurveClusters.append(cls)
+    mc.hide(chainCurveClusters)
+
+    # parent chain curve
+    mc.parent(chainCurve, rigmodule.partsNoTransGrp)
+
+    # make attach groups
+    baseAttachGrp = mc.group(n="{}BaseAttach_grp".format(prefix), em=1, p=rigmodule.partsGrp)
+    mc.delete(mc.pointConstraint(chainJoints[0], baseAttachGrp))
+
+    # make controls
+    chainControls = []
+    controlScaleIncrement = (1.0 - smallestScalePercent) / numChainCVs
+    mainCtrlScaleFactor = 5.0
+
+    for i in range(numChainCVs):
+        crtlScale = rigScale * mainCtrlScaleFactor * (1.0 - (i * controlScaleIncrement))
+        prefixNumber = "%d" % (i + 1)
+        ctrl = control.Control(prefix="{}{}".format(prefix, prefixNumber),
+                                translateTo=chainCurveClusters[i],
+                                scale=crtlScale,
+                                parent=rigmodule.controlsGrp,
+                                shape="sphere")
