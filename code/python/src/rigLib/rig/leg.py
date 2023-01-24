39ebebd02e0af930843646095fc4c56be8f681e3
Leonardo Pinheiro <60147713+leonardpin-br@users.noreply.github.com>
2023-01-24 09:03:14 -0300

    After 43

--- code/python/src/rigLib/rig/leg.py
+++ code/python/src/rigLib/rig/leg.py
@@ -34,3 +34,55 @@ def build(legJoints,
         Dictionary with rig module objects.
     """
 
+    # make rig module
+    rigmodule = module.Module(prefix=prefix, baseObj=baseRig)
+
+    # make attach groups
+    bodyAttachGrp = mc.group(n="{}BodyAttach_grp".format(prefix),
+                             em=1,
+                             p=rigmodule.partsGrp)
+    baseAttachGrp = mc.group(n="{}BaseAttach_grp".format(prefix),
+                             em=1,
+                             p=rigmodule.partsGrp)
+
+    # make controls
+
+    if scapulaJnt:
+        scapulaCtrl = control.Control(prefix="{}Scapula".format(prefix),
+                                      translateTo=scapulaJnt,
+                                      rotateTo=scapulaJnt,
+                                      scale=(rigScale * 3),
+                                      parent=rigmodule.controlsGrp,
+                                      shape="sphere",
+                                      lockChannels=["ty", "rx", "rz", "s", "v"])
+
+    footCtrl = control.Control(prefix="{}Foot".format(prefix),
+                               translateTo=legJoints[2],
+                               scale=(rigScale * 3),
+                               parent=rigmodule.controlsGrp,
+                               shape="circleY")
+
+    ballCtrl = control.Control(prefix="{}Ball".format(prefix),
+                               translateTo=legJoints[3],
+                               rotateTo=legJoints[3],
+                               scale=(rigScale * 2),
+                               parent=footCtrl.C,
+                               shape="circleZ")
+
+    poleVectorCtrl = control.Control(prefix="{}PV".format(prefix),
+                                     translateTo=pvLocator,
+                                     scale=rigScale,
+                                     parent=rigmodule.controlsGrp,
+                                     shape="sphere")
+
+    toeIkControls = []
+    for topToeJnt in topToeJoints:
+        toePrefix = name.removeSuffix(topToeJnt)[:-1]
+        toeEndJnt = mc.listRelatives(topToeJnt, ad=1, type="joint")[0]
+
+        toeIkCtrl = control.Control(prefix=toePrefix,
+                                    translateTo=toeEndJnt,
+                                    scale=rigScale,
+                                    parent=footCtrl.C,
+                                    shape="circleY")
+        toeIkControls.append(toeIkCtrl)
